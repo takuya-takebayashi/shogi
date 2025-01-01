b8e343c10a5f1080d41298a5f73a44b1323a1a34
竹林拓也 <takegoog369@gmail.com>
2025-01-02 00:55:47 +0900

    盤面生成周りの処理を追加

--- index.js
+++ index.js
@@ -21,20 +21,10 @@ const kingConf = {
   frontImage: "",
   backImage: "",
   counter: 1,
-  avilablePosition: (nowPosition) => {
-    const first = nowPosition[0];
-    const second = nowPosition[1];
-    let result = [];
-    // 左端にいる場合
-    if (0 === second) {
-      // 正面
-      if (first !== 0) {
-        if (isEmpty(boardManagement[first - 1][0])) {
-          result.push(2);
-        }
-      }
-    }
-  },
+  positionConf: {
+    "ou": ["five-one"],
+    "gyoku": ["five-nine"]
+  }
 };
 // 金の設定
 const kinConf = {
@@ -43,6 +33,10 @@ const kinConf = {
   frontImage: "",
   backImage: "",
   counter: 2,
+  positionConf: {
+    "ou": ["six-one", "four-one"],
+    "gyoku": ["six-nine", "four-nine"]
+  }
 };
 // 銀の設定
 const ginConf = {
@@ -51,6 +45,10 @@ const ginConf = {
   frontImage: "",
   backImage: "",
   counter: 2,
+  positionConf: {
+    "ou": ["seven-one", "three-one"],
+    "gyoku": ["seven-nine", "three-nine"]
+  }
 };
 // 飛車の設定
 const hisyaConf = {
@@ -59,6 +57,10 @@ const hisyaConf = {
   frontImage: "",
   backImage: "",
   counter: 1,
+  positionConf: {
+    "ou": ["seven-two"],
+    "gyoku": ["three-eight"]
+  }
 };
 // 角の設定
 const kakuConf = {
@@ -67,6 +69,10 @@ const kakuConf = {
   frontImage: "",
   backImage: "",
   counter: 1,
+  positionConf: {
+    "ou": ["three-two"],
+    "gyoku": ["seven-eight"]
+  }
 };
 // 桂馬の設定
 const keimaConf = {
@@ -75,6 +81,10 @@ const keimaConf = {
   frontImage: "",
   backImage: "",
   counter: 2,
+  positionConf: {
+    "ou": ["eight-one", "two-one"],
+    "gyoku":["eight-nine", "two-nine"]
+  }
 };
 // 香車の設定
 const kyosyaConf = {
@@ -83,6 +93,10 @@ const kyosyaConf = {
   frontImage: "",
   backImage: "",
   counter: 2,
+  positionConf: {
+    "ou": ["nine-one", "one-one"],
+    "gyoku": ["nine-nine", "one-nine"]
+  }
 };
 // 歩の設定
 const hoConf = {
@@ -91,6 +105,10 @@ const hoConf = {
   frontImage: "",
   backImage: "",
   counter: 9,
+  positionConf: {
+    "ou": ["nine-three", "eight-three", "seven-three", "six-three", "five-three", "four-three", "three-three", "two-three", "one-three"],
+    "gyoku": ["nine-seven", "eight-seven", "seven-seven", "six-seven", "five-seven", "four-seven", "three-seven", "two-seven", "one-seven"]
+  }
 };
 // 王、玉
 let owners = ["ou", "gyoku"];
@@ -104,10 +122,17 @@ const generater = (conf, owner) => {
     piece.front = conf.front;
     piece.back = conf.back;
     piece.owner = owner;
-    // console.log(piece);
+    const map = new Map(Object.entries(conf.positionConf));
+    piece.position = map.get(owner)[i];
     divideOwner(owner, piece);
   }
 };
+// 駒の初期配置設定
+const initPosition = (piece) => {
+  let result = "";
+
+}
+
 // 駒振り分け関数
 const divideOwner = (owner, piece) => {
   if ("ou" === owner) {
@@ -115,8 +140,6 @@ const divideOwner = (owner, piece) => {
   } else {
     gyokuOwnerPieces.push(piece);
   }
-  // console.log(ouOwnerPieces);
-  // console.log(gyokuOwnerPieces);
 };
 
 // ターン管理変数
@@ -151,6 +174,14 @@ const generatePieces = () => {
   }
 };
 
+// 盤面に駒を置く
+const putPiece = (piece) => {
+  const positionJPN = piece.position;
+  const [i, j] = getPiecePosition(piece);
+  let displayPosition = document.getElementById(positionJPN);
+  displayPosition.innerHTML = piece.front;
+}
+
 // 駒を初期位置へ配置する
 const setUpPieces = () => {
   // 王の駒を並べる
@@ -158,50 +189,63 @@ const setUpPieces = () => {
     if ("ou" === piece.front) {
       // 5一へ配置
       boardManagement[0].splice(4, 1, piece);
+      putPiece(piece);
     } else if ("kyosya" === piece.front) {
       if (isEmpty(boardManagement[0][0])) {
         // 9一へ配置
         boardManagement[0].splice(0, 1, piece);
+        putPiece(piece);
       } else {
         // 1一へ配置
         boardManagement[0].splice(8, 1, piece);
+        putPiece(piece);
       }
     } else if ("keima" === piece.front) {
       if (isEmpty(boardManagement[0][1])) {
         // 8一へ配置
         boardManagement[0].splice(1, 1, piece);
+        putPiece(piece);
       } else {
         // 2一へ配置
         boardManagement[0].splice(7, 1, piece);
+        putPiece(piece);
       }
     } else if ("gin" === piece.front) {
       if (isEmpty(boardManagement[0][2])) {
         // 7一へ配置
         boardManagement[0].splice(2, 1, piece);
+        putPiece(piece);
       } else {
         // 3一へ配置
         boardManagement[0].splice(6, 1, piece);
+        putPiece(piece);
       }
     } else if ("kin" === piece.front) {
       if (isEmpty(boardManagement[0][3])) {
         // 6一へ配置
         boardManagement[0].splice(3, 1, piece);
+        putPiece(piece);
       } else {
         // 4一へ配置
         boardManagement[0].splice(5, 1, piece);
+        putPiece(piece);
       }
     } else if ("kaku" === piece.front) {
       // 8二へ配置
       boardManagement[1].splice(1, 1, piece);
+      putPiece(piece);
     } else if ("hisya" === piece.front) {
       // 2二へ配置
       boardManagement[1].splice(7, 1, piece);
+      putPiece(piece);
     } else if ("ho" === piece.front) {
       if (isEmpty(boardManagement[2][0])) {
         boardManagement[2].splice(0);
+        putPiece(piece);
       }
       // 9三～9三へ配置する
       boardManagement[2].push(piece);
+      putPiece(piece);
     }
   }
   // 玉の駒を並べる
@@ -209,57 +253,102 @@ const setUpPieces = () => {
     if ("ou" === piece.front) {
       // 5九へ配置
       boardManagement[8].splice(4, 1, piece);
+      putPiece(piece);
     } else if ("kyosya" === piece.front) {
       if (isEmpty(boardManagement[8][0])) {
         // 9九へ配置
         boardManagement[8].splice(0, 1, piece);
+        putPiece(piece);
       } else {
         // 1九へ配置
         boardManagement[8].splice(8, 1, piece);
+        putPiece(piece);
       }
     } else if ("keima" === piece.front) {
       if (isEmpty(boardManagement[8][1])) {
         // 8九へ配置
         boardManagement[8].splice(1, 1, piece);
+        putPiece(piece);
       } else {
         // 2九へ配置
         boardManagement[8].splice(7, 1, piece);
+        putPiece(piece);
       }
     } else if ("gin" === piece.front) {
       if (isEmpty(boardManagement[8][2])) {
         // 7九へ配置
         boardManagement[8].splice(2, 1, piece);
+        putPiece(piece);
       } else {
         // 3九へ配置
         boardManagement[8].splice(6, 1, piece);
+        putPiece(piece);
       }
     } else if ("kin" === piece.front) {
       if (isEmpty(boardManagement[8][3])) {
         // 6九へ配置
         boardManagement[8].splice(3, 1, piece);
+        putPiece(piece);
       } else {
         // 4九へ配置
         boardManagement[8].splice(5, 1, piece);
+        putPiece(piece);
       }
     } else if ("kaku" === piece.front) {
       // 8八へ配置
       boardManagement[7].splice(1, 1, piece);
+      putPiece(piece);
     } else if ("hisya" === piece.front) {
       // 2八へ配置
       boardManagement[7].splice(7, 1, piece);
+      putPiece(piece);
     } else if ("ho" === piece.front) {
       if (isEmpty(boardManagement[6][0])) {
         boardManagement[6].splice(0);
+        putPiece(piece);
       }
       // 9七～9七へ配置する
       boardManagement[6].push(piece);
+      putPiece(piece);
     }
   }
+  console.log(boardManagement);
+
+  // 
+
 };
 // オブジェクトの空判定
 const isEmpty = (object) => {
   return Object.keys(object).length === 0;
 };
+
+// 将棋盤の配置設定
+const boardConfigJPN = [
+  ["nine-one", "eight-one", "seven-one", "six-one", "five-one", "four-one", "three-one", "two-one", "one-one"],
+  ["nine-two", "eight-two", "seven-two", "six-two", "five-two", "four-two", "three-two", "two-two", "one-two"],
+  ["nine-three", "eight-three", "seven-three", "six-three", "five-three", "four-three", "three-three", "two-three", "one-three"],
+  ["nine-four", "eight-four", "seven-four", "six-four", "five-four", "four-four", "three-four", "two-four", "one-four"],
+  ["nine-five", "eight-five", "seven-five", "six-five", "five-five", "four-five", "three-five", "two-five", "one-five"],
+  ["nine-six", "eight-six", "seven-six", "six-six", "five-six", "four-six", "three-six", "two-six", "one-six"],
+  ["nine-seven", "eight-seven", "seven-seven", "six-seven", "five-seven", "four-seven", "three-seven", "two-seven", "one-seven"],
+  ["nine-eight", "eight-eight", "seven-eight", "six-eight", "five-eight", "four-eight", "three-eight", "two-eight", "one-eight"],
+  ["nine-nine", "eight-nine", "seven-nine", "six-nine", "five-nine", "four-nine", "three-nine", "two-nine", "one-nine"]
+];
+
+// 駒のポジションから盤面管理配列内の場所を返す
+const getPiecePosition = (piece) =>{
+  const position = piece.position;
+  let result = [];
+  for (let i = 0; i < boardConfigJPN.length; i++) {
+    if(boardConfigJPN[i].some(item => item === position)){
+      let j = boardConfigJPN[i].findIndex(item => item === position);
+      result.push(i);
+      result.push(j);
+    }
+  }
+  return result;
+}
+
 // 盤面生成処理
 const generateBoard = () => {
   console.log("盤面の生成を始めます");
@@ -278,6 +367,19 @@ const generateBoard = () => {
       new Object(),
     ];
     boardManagement.push(array);
+  }
+  let lines = ["-one", "-two", "-three", "-four", "-five", "-six", "-seven", "-eight", "-nine"]
+  let columns = ["nine", "eight", "seven", "six", "five", "four", "three", "two", "one"];
+  for(line of lines){
+    for(column of columns){
+      let positon = column + line;
+      let element = document.createElement("div");
+      element.id = positon;
+      element.className = "cell"
+      element.classList.add(line);
+      element.innerHTML = "1";
+      board.appendChild(element);
+    }
   }
 };
 
